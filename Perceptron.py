e47f6ff063cf92dc29e4141e2ba5a9716cb5187c
xiaoguzai <31726901+boss2020@users.noreply.github.com>
2020-06-27 09:19:17 +0800

    Update Perceptron.py

--- Perceptron.py
+++ Perceptron.py
@@ -1,40 +1,28 @@
-# -*- coding: utf-8 -*-
 import copy
-
+import time
 trainint_set = [[(3,3),1],[(4,3),1],[(1,1),-1]]       #输入数据
 w = [0,0]          #初始化w参数
 b = 0              #初始化b参数
-
-def update(item):
-    global w,b
-    w[0] += 1*item[1]*item[0][0]               #w的第一个分量更新
-    w[1] += 1*item[1]*item[0][1]               #w的第二个分量更新
-    b += 1*item[1]
-    print('w = [%d,%d],b = %d'%(w[0],w[1],b))                     #打印出结果
-
-def judge(item):                               #返回y = yi(w*x+b)的结果
-    res = 0
-    for i in range(len(item[0])):
-        res +=item[0][i]*w[i]                   #对应公式w*x
-    res += b                                    #对应公式w*x+b
-    res *= item[1]                              #对应公式yi(w*x+b)
-    return res
-
-def check():                                    #检查所有数据点是否分对了
-    flag = False
-    for item in trainint_set:
-        if judge(item)<=0:                       #如果还有误分类点，那么就小于等于0
-            flag = True
-            update(item)                         #只要有一个点分错，我就更新
-    return flag                                  #flag为False，说明没有分错的了
-
-if __name__ == '__main__':
-    flag = False
-    for i in range(1000):
-        if not check():                            #如果已经没有分错的话
-            flag = True
-            break
-    if flag:
-        print('在1000次以内全部分对了')
+#测试集中的数据始终不发生变化
+def  change(item):
+    global  w,b
+    w[0] = w[0]+item[1]*item[0][0]
+    w[1] = w[1]+item[1]*item[0][1]
+    b = b+item[1]
+#使用梯度下降算法更新每次前面的系数,每次更新的时候w = w+y*x,b = b+y
+def  judge(item):
+    result = item[1]*(item[0][0]*w[0]+item[0][1]*w[1]+b)
+    if  result > 0:
+        return  True
     else:
-        print('很不幸，1000次迭代还是没有分对')
+        return  False
+if __name__ == '__main__':
+    cur = 0
+    while cur < len(trainint_set):
+        item = trainint_set[cur]
+        if  judge(item) == False:
+            change(item)
+            cur = 0
+        else:
+            cur = cur+1
+    print('results = %d,%d,%d'%(w[0],w[1],b))
